9a4adbda37dc23610bb2e0843c46661d109df170
eugeneshark <yevhenii.skakun@gmail.com>
2021-10-03 17:37:00 +0200

    Added urls to other applications

--- cipher/cipher/urls.py
+++ cipher/cipher/urls.py
@@ -15,7 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import include
+
+from cipher import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('pages.urls')),
 ]
+
